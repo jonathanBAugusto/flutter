dfbca0ac290b8d37d45250b1a5164c4a17218103
Jhon Doe Dev <jonathanbersot21@gmail.com>
2019-05-29 18:06:17 -0300

    up 29052019
    List Worked!

--- main - todo_list.dart
+++ main - todo_list.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
@@ -29,47 +30,110 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List _toDoList = [];
   Color _cbground = Colors.greenAccent;
+  Color _cbground2 = Colors.lightGreen;
   Color _ctext = Colors.white;
-  int count = 0;
   final _edDescription = TextEditingController();
 
-  void _addToDo() {
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _setColorTheme();
+    });
+    _readFile().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _toDoAdd() {
     setState(() {
       Map<String, dynamic> newTodo = Map();
-      newTodo["title"] = _edDescription.text;
+      newTodo["title"] = _edDescription.text.toString();
       _edDescription.text = "";
       newTodo["ok"] = false;
       _toDoList.add(newTodo);
+      _saveFile(false);
     });
-    // _saveFile();
   }
 
-  void setColorTheme() {
+  void _deleteAll(bool del) => setState(() {
+        _saveFile(del);
+      });
+
+  void _setColorTheme() {
     setState(() {
+      var random = Random.secure();
+      int count = random.nextInt(6);
       switch (count) {
         case 0:
           _cbground = Colors.greenAccent;
+          _cbground2 = Colors.lightGreen;
           _ctext = Colors.white;
           break;
         case 1:
           _cbground = Colors.blueAccent;
+          _cbground2 = Colors.lightBlue;
           _ctext = Colors.white;
           break;
         case 2:
-          _cbground = Colors.purpleAccent;
+          _cbground = Colors.deepPurple;
+          _cbground2 = Colors.purple;
           _ctext = Colors.white;
           break;
         case 3:
           _cbground = Colors.redAccent;
+          _cbground2 = Colors.orangeAccent;
           _ctext = Colors.white;
           break;
+        case 4:
+          _cbground = Colors.black;
+          _cbground2 = Colors.grey;
+          _ctext = Colors.white;
+          break;
+        case 5:
+          _cbground = Colors.black45;
+          _cbground2 = Colors.black38;
+          _ctext = Colors.black87;
+          break;
+        case 6:
+          _cbground = Colors.black38;
+          _cbground2 = Colors.black26;
+          _ctext = Colors.cyan;
+          break;
       }
-      if (count < 3)
-        count++;
-      else
-        count = 0;
     });
   }
+
+  SimpleDialog dialog(String title) {
+    return SimpleDialog(
+      backgroundColor: _cbground,
+      title: Text(title),
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Expanded(child: Text("")),
+            RaisedButton(
+              color: _cbground,
+              child: Text("Sim"),
+              textColor: _ctext,
+              onPressed: _pressedSim,
+            ),
+            RaisedButton(
+              color: _cbground,
+              child: Text("Não"),
+              textColor: _ctext,
+              onPressed: _pressedNao,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void _pressedSim() => _deleteAll(true);
+  void _pressedNao() => _deleteAll(false);
 
   @override
   Widget build(BuildContext context) {
@@ -108,7 +172,7 @@ class _HomeState extends State<Home> {
                   color: _cbground,
                   child: Text("ADD"),
                   textColor: _ctext,
-                  onPressed: _addToDo,
+                  onPressed: _toDoAdd,
                 ),
               ],
             )),
@@ -116,29 +180,55 @@ class _HomeState extends State<Home> {
           child: ListView.builder(
               padding: EdgeInsets.only(top: 10),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_toDoList[index].task),
-                  value: _toDoList[index].ok,
-                  secondary: CircleAvatar(
-                    child:
-                        Icon(_toDoList[index].ok ? Icons.check : Icons.error),
-                  ),
-                  onChanged: (bool value) {
-                    setState(() {
-                      _toDoList[index]["ok"] = value;
-                    });
-                  },
-                );
-              }),
+              itemBuilder: buildItem),
         ),
-        RaisedButton(
-          color: _cbground,
-          child: Text("ChangeColor"),
-          textColor: _ctext,
-          onPressed: setColorTheme,
-        )
+        Row(
+          children: <Widget>[
+            RaisedButton(
+              color: _cbground,
+              child: Text("ChangeColor"),
+              textColor: _ctext,
+              onPressed: _setColorTheme,
+            ),
+            RaisedButton(
+              color: _cbground,
+              child: Text("Delete All"),
+              textColor: _ctext,
+              onPressed: () => dialog("Delete All").build(context),
+            ),
+          ],
+        ),
       ],
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: _cbground2,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: _ctext),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        activeColor: _cbground,
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+          foregroundColor: _ctext,
+          backgroundColor: _cbground,
+        ),
+        onChanged: (bool value) {
+          setState(() {
+            _toDoList[index]["ok"] = value;
+            _saveFile(false);
+          });
+        },
+      ),
     );
   }
 
@@ -147,7 +237,8 @@ class _HomeState extends State<Home> {
     return File("${directory.path}/data.json");
   }
 
-  Future<File> _saveFile() async {
+  Future<File> _saveFile(bool delall) async {
+    if (delall) _toDoList = List();
     String data = json.encode(_toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
